521e1a8b7b4d3180aec12360f3d1a48e41b7aa6c
Prakash251299 <prakashpratap2512@gmail.com>
2023-07-03 09:45:48 +0530

    Modified file 912SortArrayAndItsIndexCountSort.cpp
    Description: Sorted index is found by inserting elements with its indexes in hashmap before sorting and stored the indexes in a vector according to the sorted array after sorting

--- Problems/912SortArrayAndItsIndexCountSort.cpp
+++ Problems/912SortArrayAndItsIndexCountSort.cpp
@@ -6,21 +6,9 @@
 using namespace std;
 vector<int> countSort(vector<int> &v){
 	unordered_map<int,int> m;
-	unordered_map<int,vector<int>> indexCont;
 	vector<int>vs;
 	int max = *max_element(v.begin(),v.end());
 	int min = *min_element(v.begin(),v.end());
-	/* Stores index of elements of the vector in hashmap (indexCont) */
-	for(int i=0;i<v.size();i++){
-			indexCont[v[i]].push_back(i);
-	}
-//	/* Prints elements of indexCont */
-//	for(auto x:indexCont){
-//		cout<<x.first<<":";
-//		for(auto y:x.second){
-//			cout<<y<<" ";
-//		}
-//	}
 	/* Stores count of elements of the vector in hashmap (m) */
 	for(auto x:v){
 		if(m.find(x)!=m.end()){
@@ -39,26 +27,31 @@ vector<int> countSort(vector<int> &v){
 			}
 		}
 	}
-	/* Getting the sorted index and storing it into a vector */
-	for(int i=0;i<v.size();i++){
-		if(i>0 and v[i]!=v[i-1]){
-			for(auto y:indexCont[v[i]]){
-				vs.push_back(y);
-			}
-		}else{
-			if(i==0){
-				for(auto y:indexCont[v[i]]){
-					vs.push_back(y);
-				}
-			}
-		}
-	}
 	return vs;
 }
 int main(){
 	vector<int> v = {32,43,5,23,53,5};
+	unordered_map<int,vector<int>>m;
 	vector<int> sortedIndex;
-	sortedIndex = countSort(v);
+	/* Storing indexes into hashmap */
+	for(int i=0;i<v.size();i++){
+		m[v[i]].push_back(i);
+	}
+	/* Sort is called */
+	countSort(v);
+	/* Storing sorted indexes into a vector */
+	for(int j=0;j<v.size();j++){
+		if(j==0){
+			for(auto x:m[v[j]]){
+				sortedIndex.push_back(x);
+			}
+		}
+		if(j>0 and v[j]!=v[j-1]){
+			for(auto x:m[v[j]]){
+				sortedIndex.push_back(x);
+			}
+		}
+	}
 	/* For printing the sorted array */
 	cout<<"Sorted Array: ";
 	for(auto x:v){
