1be7d763741bad6294d3cad25bb60e661b6ca697
Prakash251299 <88026044+Prakash251299@users.noreply.github.com>
2024-01-14 15:39:13 +0530

    Update 2385AmountOfTimeForBinaryTreeToBeInfected.cpp

--- Problems/2385AmountOfTimeForBinaryTreeToBeInfected.cpp
+++ Problems/2385AmountOfTimeForBinaryTreeToBeInfected.cpp
@@ -27,113 +27,39 @@ public:
         traverse(q->right,m);
         return;
     }
+
     int max=0;
-    // void infect(unordered_map<int,vector<int>>m,int h,int prev,int x){
-    //     // cout<<h<<" ";
-    //     if(h>max){
-    //         max = h;
-    //     }
-    //     if(m[x].size()>1){
-    //         for(auto y:m[x]){
-    //             if(y!=prev){
-    //                 infect(m,h+1,x,y);
-    //             }
-    //             if(y==prev){
-    //                 continue;
-    //             }
-    //         }
-    //     }
-    //     // if(){
-            
-    //     // }
-    //     return;
-    // }
+
+    void infected(unordered_map<int,vector<int>>m,unordered_set<int>&visited,int start){
+        queue<int>q;
+        q.push(start);
+        visited.insert(start);
+        int minute=0;
+        while(!q.empty()){
+            int size = q.size();
+            while(size>0){
+                int curr = q.front();
+                q.pop();
+                for(auto x:m[curr]){
+                    if(visited.find(x)==visited.end()){
+                        q.push(x);
+                        visited.insert(x);
+                    }
+                }
+                size--;
+            }
+            minute++;
+        }
+        max = minute-1;
+        return;
+    }
 
     int amountOfTime(TreeNode* root, int start) {
         int f=0,h=0,prev = start;
         unordered_map<int,vector<int>>m;
         traverse(root,m);
-        // for(auto x:m){
-        //     cout<<x.first<<":";
-        //     for(auto y:x.second){
-        //         cout<<y<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-
-        // cout<<m[start].size();
-        // cout<<m[start];
-
-
-
-
-
-
-
-
-        queue<int> q;
-        q.push(start);
-        int minute = 0;
-        unordered_set<int> visited;
-        visited.insert(start);
-
-        while (!q.empty()) {
-            int levelSize = q.size();
-            while (levelSize > 0) {
-                int current = q.front();
-                q.pop();
-
-                for (int num : m[current]) {
-                    if (visited.find(num) == visited.end()) {
-                        visited.insert(num);
-                        q.push(num);
-                    }
-                }
-                levelSize--;
-            }
-            minute++;
-        }
-        return minute - 1;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // queue<int>q;
-        // unordered_set<int>visited;
-        // q.push(start);
-        // int min = -1;
-        // while(!q.empty()){
-        //     min++;
-        //     int n = q.size();
-        //     while(n--){
-        //         int curr = q.front();
-        //         q.pop();
-        //         for(int &ng:m[curr]){
-        //             if(visited.find(ng)==visited.end()){
-        //                 q.push(ng);
-        //                 visited.insert(ng);
-        //             }
-        //         }
-        //     }
-        //     min++;
-        // }
-        // return min-1;
-
-
-        // for(auto x:m[start]){
-        //     infect(m,h+1,prev,x);
-        //     // cout<<x<<" ";
-        // }
-        // return max;
+        unordered_set<int>visited;
+        infected(m,visited,start);
+        return max;
     }
 };
