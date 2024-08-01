89546c79c8972c297ba8d754f316c74313406ceb
Prakash251299 <88026044+Prakash251299@users.noreply.github.com>
2024-08-01 10:32:54 +0530

    Update 2678NumberOfSeniorCitizens.cpp

--- Problems/2678NumberOfSeniorCitizens.cpp
+++ Problems/2678NumberOfSeniorCitizens.cpp
@@ -1,1 +1,17 @@
-hi
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int c=0;
+        for(int i=0;i<details.size();i++){
+            int a=details[i][11]-'0';
+
+            if(int(details[i][11])-'0'>6){
+                c++;
+            }
+            if(a==6 && details[i][12]-'0'>0){
+                c++;
+            }
+        }
+        return c;
+    }
+};
