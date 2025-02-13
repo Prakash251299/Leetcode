7808347749f0becea0e8e0a9a14b6eb59fd3cf91
Prakash251299 <88026044+Prakash251299@users.noreply.github.com>
2025-02-13 19:22:57 +0530

    Update 310MinimumHeightTrees.cpp

--- Problems/310MinimumHeightTrees.cpp
+++ Problems/310MinimumHeightTrees.cpp
@@ -6,16 +6,10 @@ public:
         }
         unordered_map<int,vector<int>>m;
         vector<int>v;
-        vector<int>visited(n,0);
-        int height=0;
-        
-        int min = pow(10,5);
-        int endLeaf = 0;
         for(int i=0;i<edges.size();i++){
             m[edges[i][0]].push_back(edges[i][1]);
             m[edges[i][1]].push_back(edges[i][0]);
         }
-        // display(m);
         unordered_map<int,int>count;
         queue<int>leaf;
 
@@ -24,7 +18,6 @@ public:
             count[x.first] = x.second.size();
             if(x.second.size()==1){
                 leaf.push(x.first);
-            //     visited[x.first] = 1;
             }
         }
         while(1){
@@ -35,7 +28,6 @@ public:
             n-=leafSize;
             int i=0;
             while(i<leafSize){
-                --count[leaf.front()];
                 for(auto x:m[leaf.front()]){
                     --count[x];
                     if(count[x]==1){
@@ -46,7 +38,6 @@ public:
                 i++;
             }
         }
-        // display(leaf);
         while(!leaf.empty()){
             v.push_back(leaf.front());
             leaf.pop();
