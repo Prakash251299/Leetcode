43a19947ff55ec62b271c94fb27810d2b49a4ff9
Prakash251299 <prakashpratap2512@gmail.com>
2025-06-09 21:22:26 +0530

    Added 792NumberOfMatchingSubsequences(using_map_TLE).cpp

---
+++ Problems/792NumberOfMatchingSubsequences(using_map_TLE).cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    void display(vector<map<char,int>>&v){
+        for(int i=0;i<v.size();i++){
+            cout<<"ind:"<<i<<"\n";
+            for(auto y:v[i]){
+                cout<<y.first<<":"<<y.second<<"\n";
+            }
+            cout<<"\n";
+        }
+        cout<<"\n";
+    }
+    void display(vector<string>&v){
+        for(auto x:v){
+            cout<<x<<" ";
+        }
+        cout<<"\n";
+    }
+    int searchWord(vector<map<char,int>>&m,string &s,char &c){
+    // int searchWord(vector<vector<int>>&m,string &s,char &c){
+        // cout<<"searchedWord: "<<s<<"\n";
+        // int curr=1,i=0;
+        // if(c==s[0]){
+        //     i++;
+        // }
+        // for(;i<s.size();i++){
+        //     if(m[curr-1][int(s[i])-97]==0){
+        //         return 0;
+        //     }
+        //     curr = m[curr-1][s[i]-97];
+        // }
+        // return 1;
+        int curr=1,i=0;
+        if(c==s[0]){
+            i++;
+        }
+        for(;i<s.size();i++){
+            if(m[curr-1][s[i]]==0){
+                return 0;
+            }
+            curr = m[curr-1][s[i]];
+        }
+        return 1;
+    }
+    int numMatchingSubseq(string s, vector<string>& words) {
+        vector<map<char,int>>v(s.size());
+        map<char,int>m;
+        // vector<vector<int>>v(s.size(),vector<int>(26,0));
+        // vector<int>temp(26,0);
+        if(s.size()>1){
+            for(int i=s.size()-2;i>=0;i--){
+                m[s[i+1]] = i+2;
+                v[i]=(m);
+                // temp[s[i+1]-97]=i+2;
+                // v[i]=temp;
+            }
+        }else{
+            v[0][s[0]]=1;
+        }
+        // return 0;
+        // display(v);
+        vector<string>res;
+        int count=0;
+        for(auto x:words){
+            if(searchWord(v,x,s[0])){
+                res.push_back(x);
+                count++;
+            }
+        }
+        // display(res);
+        return count;
+    }
+};
