c63ddc1a93e99240bf314d6ee52aa79b41e58cd3
Prakash251299 <prakashpratap2512@gmail.com>
2025-06-09 21:24:21 +0530

    Update 792NumberOfMatchingSubsequences(using_map_TLE).cpp

--- Problems/792NumberOfMatchingSubsequences(using_map_TLE).cpp
+++ Problems/792NumberOfMatchingSubsequences(using_map_TLE).cpp
@@ -17,19 +17,6 @@ public:
         cout<<"\n";
     }
     int searchWord(vector<map<char,int>>&m,string &s,char &c){
-    // int searchWord(vector<vector<int>>&m,string &s,char &c){
-        // cout<<"searchedWord: "<<s<<"\n";
-        // int curr=1,i=0;
-        // if(c==s[0]){
-        //     i++;
-        // }
-        // for(;i<s.size();i++){
-        //     if(m[curr-1][int(s[i])-97]==0){
-        //         return 0;
-        //     }
-        //     curr = m[curr-1][s[i]-97];
-        // }
-        // return 1;
         int curr=1,i=0;
         if(c==s[0]){
             i++;
@@ -45,25 +32,20 @@ public:
     int numMatchingSubseq(string s, vector<string>& words) {
         vector<map<char,int>>v(s.size());
         map<char,int>m;
-        // vector<vector<int>>v(s.size(),vector<int>(26,0));
-        // vector<int>temp(26,0);
         if(s.size()>1){
             for(int i=s.size()-2;i>=0;i--){
                 m[s[i+1]] = i+2;
                 v[i]=(m);
-                // temp[s[i+1]-97]=i+2;
-                // v[i]=temp;
             }
         }else{
             v[0][s[0]]=1;
         }
-        // return 0;
         // display(v);
-        vector<string>res;
+        // vector<string>res;
         int count=0;
         for(auto x:words){
             if(searchWord(v,x,s[0])){
-                res.push_back(x);
+                // res.push_back(x);
                 count++;
             }
         }
