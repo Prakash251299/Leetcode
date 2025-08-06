f5ec803aff9a198178c7d7d5096e21146b322171
Prakash251299 <prakashpratap2512@gmail.com>
2025-08-06 23:40:52 +0530

    Added 46Permutations.cpp

---
+++ Problems/46Permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void fun(vector<int>& nums, set<vector<int>>&res,unordered_map<int,int>&m,vector<int>&v,int c){
+        if(c==nums.size()){
+            //cout<<v.size();
+            res.insert(v);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(m[i]==1){continue;}
+            else{m[i]=1;}
+            c++;
+            v.push_back(nums[i]);
+            fun(nums,res,m,v,c);
+            m[i]=0;
+            c--;
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>res;
+        set<vector<int>>s;
+        unordered_map<int,int>m;
+        vector<int>v;
+        fun(nums,s,m,v,0);
+        for(auto x:s){
+            res.push_back(x);
+        }
+        return res;
+    }
+};
