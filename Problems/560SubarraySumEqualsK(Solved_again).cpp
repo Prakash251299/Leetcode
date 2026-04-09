439deecf4e926b79808cc955f55945e163aec2b8
Prakash251299 <prakashpratap2512@gmail.com>
2026-04-09 15:10:01 +0530

    Refactor subarraySum to use prefix sums

--- Problems/560SubarraySumEqualsK(Solved_again).cpp
+++ Problems/560SubarraySumEqualsK(Solved_again).cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        vector<int>v;
-        int sum=0;
-        for(auto x:nums){
-            sum+=x;
-            v.push_back(sum);
+        vector<int>pr(nums.size(),0);
+        pr[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            pr[i]=pr[i-1]+nums[i];
         }
-        map<int,int>m;
+        unordered_map<int,int>m;
+        int c=0;
         m[0]=1;
-        int res=0;
-        for(auto x:v){
-            res+=m[x-k];
-            ++m[x];
+        for(int i=0;i<pr.size();i++){
+            if(m[pr[i]-k]>0){
+                c+=m[pr[i]-k];
+            }
+            m[pr[i]]++;
         }
-        return res;
+        return c;
     }
 };
