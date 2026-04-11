09280d980061e6a8d809055d601f622600d606bd
Prakash251299 <prakashpratap2512@gmail.com>
2026-04-11 17:03:15 +0530

    Fix logic for counting nice subarrays

--- Problems/1248CountNumberOfNiceSubarrays[time_O(n)_space_O(1)].cpp
+++ Problems/1248CountNumberOfNiceSubarrays[time_O(n)_space_O(1)].cpp
@@ -4,18 +4,18 @@ public:
         int i=0,odd=0,res=0,temp=0;
         for(int j=0;j<nums.size();j++){
             if(nums[j]%2!=0){
-                if(odd<k)
                 odd++;
-                if(odd>=k){
+                if(odd==k){
                     temp=0;
-                    while(1){
+                    while(i<=j){
                         temp++;
-                        if(nums[i]%2!=0 || i>j){
+                        if(nums[i]%2!=0){
                             i++;break;
                         }
                         i++;
                     }
                     res+=temp;
+                    odd--;
                 }
             }else{
                 res+=temp;
