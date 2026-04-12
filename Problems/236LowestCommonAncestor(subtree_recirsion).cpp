1a43c62b3cf22252539ea2f0be3678bec258b25a
Prakash251299 <prakashpratap2512@gmail.com>
2026-04-12 20:49:57 +0530

    Implement lowest common ancestor for binary tree

---
+++ Problems/236LowestCommonAncestor(subtree_recirsion).cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* traverse(TreeNode* root, TreeNode* p, TreeNode *q){
+        if(root==NULL||root==p||root==q){
+            return root;
+        }
+        
+        TreeNode* l=traverse(root->left,p,q);
+        TreeNode* r=traverse(root->right,p,q);
+        if((l==p||l==q)&&(r==p||r==q)){
+            return root;
+        }
+        return l!=NULL?l:r;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return traverse(root,p,q);
+    }
+};
