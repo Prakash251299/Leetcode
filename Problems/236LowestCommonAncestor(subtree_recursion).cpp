a21bf60ed63a905503b80210897565f8c11fe44a
Prakash251299 <prakashpratap2512@gmail.com>
2026-04-13 05:42:15 +0530

    Refactor traverse function for lowest common ancestor

--- Problems/236LowestCommonAncestor(subtree_recursion).cpp
+++ Problems/236LowestCommonAncestor(subtree_recursion).cpp
@@ -9,17 +9,13 @@
  */
 class Solution {
 public:
-    TreeNode* traverse(TreeNode* root, TreeNode* p, TreeNode *q){
-        if(root==NULL||root==p||root==q){
-            return root;
-        }
-        
-        TreeNode* l=traverse(root->left,p,q);
-        TreeNode* r=traverse(root->right,p,q);
-        if((l==p||l==q)&&(r==p||r==q)){
-            return root;
-        }
+    TreeNode* traverse(TreeNode*root,TreeNode*p,TreeNode*q){
+        if(root==NULL or root==p or root==q){return root;}
+        TreeNode* l = traverse(root->left,p,q);
+        TreeNode* r = traverse(root->right,p,q);
+        if(l!=NULL&&r!=NULL)return root;
         return l!=NULL?l:r;
+
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         return traverse(root,p,q);
