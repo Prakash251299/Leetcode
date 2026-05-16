6f6e690492de74125c3a3f7a5f4ce94781d3cb2f
Prakash251299 <prakashpratap2512@gmail.com>
2026-05-16 21:56:46 +0530

    Create 3914MinimumOperationsToMakeArrayNonDecreasing_O(n).cpp

---
+++ Problems/3914MinimumOperationsToMakeArrayNonDecreasing_O(n).cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long minOperations(vector<int>& nums) {
+        long long res=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<nums[i-1])
+            res+=nums[i-1]-nums[i];
+        }
+        return res;
+    }
+};
