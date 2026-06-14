07f4d38d6821784e13be1ddc90aec0f3ba900fca
Prakash251299 <prakashpratap2512@gmail.com>
2026-06-14 20:35:48 +0530

    Update 560SubarraySumEqualsK.cpp

--- Problems/560SubarraySumEqualsK.cpp
+++ Problems/560SubarraySumEqualsK.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-
-        int sum=0,count=0,start=0;
         unordered_map<int,int>m;
+        int c=0;
+        int sum=0;
         m[0]=1;
-        // for(auto x:m){
-        //     cout<<x.first<<":"<<x.second<<"\n";
-        // }
-        // cout<<m[-1];
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(m[sum-k]>0){
-                count+=m[sum-k];
+            if(m[sum-k]>=1){
+                c+=m[sum-k];
             }
-            ++m[sum];
+            m[sum]++;
         }
-        // for(auto x:m){
-        //     cout<<x.first<<":"<<x.second<<"\n";
-        // }
-        // for(int i=0;i<nums.size();i++){
-        //     sum+=nums[i];
-        //     if(sum==k){
-        //         count++;
-        //         sum-=nums[start];
-        //         start++;
-        //     }
-        //     while(sum>k){
-        //         sum-=nums[start];
-        //         if(sum==k && start!=i){
-        //             count++;
-        //         }
-        //         start++;
-        //     }
-        // }
-        return count;
+        return c;
     }
 };
