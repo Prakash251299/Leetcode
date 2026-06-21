111ef2112d413e3707210639fcd6c713d492ad28
Prakash251299 <prakashpratap2512@gmail.com>
2026-06-21 15:30:21 +0530

    Implement countRangeSum function using BIT

---
+++ Problems/327CountOfRangeSum(O(nlogn)).cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    void display(unordered_map<int,int>&rank){
+        for(auto x:rank){
+            cout<<x.first<<":"<<x.second<<" ";
+        }
+        cout<<endl;
+    }
+    void display(vector<int>&bit){
+        for(auto x:bit){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+    void propagate(long long sum,unordered_map<int,int>&rank,vector<int>&bit,int n){
+        long long l=rank[sum];
+        while(l<n){
+            bit[l]++;
+            l+=l&-l;
+        }
+    }
+
+    long long findCountInRange(long long &sum, int low, int up, unordered_map<int,int>&rank,vector<int>&bit){
+        long long c1=0,c2=0,l=rank[sum-low],u=rank[sum-up]-1;
+        while(l>0){
+            c1+=bit[l];
+            l-=l&-l;
+        }
+        while(u>0){
+            c2+=bit[u];
+            u-=u&-u;
+        }
+        return c1-c2;
+    }
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        unordered_map<int,int>rank;
+        set<long long>pset;
+        vector<long long>psum;
+        long long ps=0;
+        pset.insert(0);
+        for(auto x:nums){
+            ps+=x;
+            pset.insert(ps);
+            pset.insert(ps-lower);
+            pset.insert(ps-upper);
+        }
+        for(auto x:pset){
+            psum.push_back(x);
+        }
+        sort(psum.begin(),psum.end());
+        int i=1;
+        for(auto x:psum){
+            rank[x]=i;
+            i++;
+        }
+        vector<int>bit(psum.size()+1,0);
+        int n = psum.size()+1;
+        propagate(0,rank,bit,n);
+        long long sum=0,res=0;
+        for(auto x:nums){
+            sum += x;
+            long long val = findCountInRange(sum,lower,upper,rank,bit);
+            res += val;
+            propagate(sum,rank,bit,n);
+        }
+        return res;
+    }
+};
